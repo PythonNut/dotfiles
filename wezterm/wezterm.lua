1df2334afa3010de4804156502d14223c1789860
PythonNut <PythonNut@users.noreply.github.com>
2022-11-06 17:50:42 -0800

    wezterm: reduce font size, add inactive tab edges

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,18 +8,20 @@ base2 = '#eee8d5'
 
 return {
    font = wezterm.font 'Dejavu Sans Mono',
-   font_size = 10.0,
+   font_size = 9.0,
    color_scheme = "Solarized Dark (base16)",
    hide_tab_bar_if_only_one_tab = true,
    window_frame = {
       font = wezterm.font 'Dejavu Sans',
-      font_size = 10.0,
+      font_size = 9.0,
       active_titlebar_bg = base02,
       inactive_titlebar_bg = base02,
    },
    colors = {
+      cursor_bg = base1,
+      cursor_fg = base02,
       tab_bar = {
-         inactive_tab_edge = base03,
+         inactive_tab_edge = base01,
          active_tab = {
             bg_color = base03,
             fg_color = base2,
